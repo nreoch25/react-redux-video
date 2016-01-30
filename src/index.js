2b6a59ebaa0f5055d3649fa15ee8475a0b83a4e4
Nigel Reoch <nigel.reoch@cbc.ca>
2016-01-30 16:17:04 -0500

    Added video list

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import SearchBar from "./components/search_bar";
-
+import VideoList from "./components/video_list";
+import YTSearch from "youtube-api-search";
 const API_KEY = "AIzaSyDzhLqeZF6dl6MPiapm2FRtlUc9s4FvX8c";
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] };
+  }
+
+  componentDidMount() {
+    YTSearch({key: API_KEY, term: "tony romo"}, (data) => {
+      this.setState({ videos: data });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  } 
 }
 
 ReactDOM.render(<App />, document.querySelector(".container"));
