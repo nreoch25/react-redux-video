13f66790b357a237eb2ee8f1a57c9a47816c9331
Nigel Reoch <nigel.reoch@cbc.ca>
2016-01-30 16:31:26 -0500

    Display first video

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import SearchBar from "./components/search_bar";
 import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
 import YTSearch from "youtube-api-search";
 const API_KEY = "AIzaSyDzhLqeZF6dl6MPiapm2FRtlUc9s4FvX8c";
 
@@ -22,6 +23,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
         <VideoList videos={this.state.videos} />
       </div>
     );
